317368cd23ef46a9f431dfcea89a17174acf16f1
JannickWAndresen <148877603+JannickWAndresen@users.noreply.github.com>
2024-01-08 10:11:04 +0100

    Added basic ui elements.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.mediaplayerproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
