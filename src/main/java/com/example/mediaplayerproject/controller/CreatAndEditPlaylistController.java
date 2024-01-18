ab1d34a7f4b6e073073d7c1b4c5b3ca61d20d9ae
Byndum <Mikael.List-Jensen@hotmail.dk>
2024-01-18 12:48:13 +0100

    removed redundant file. added ref to buttons and controls. added refresh, and ability to choose a playlist

--- src/main/java/com/example/mediaplayerproject/controller/CreatAndEditPlaylistController.java
+++
@@ -1,5 +0,0 @@
-package com.example.mediaplayerproject.controller;
-
-public class CreatAndEditPlaylistController {
-
-}
---
+++ src/main/java/com/example/mediaplayerproject/controller/CreateAndEditPlaylistController.java
@@ -0,0 +1,87 @@
+package com.example.mediaplayerproject.controller;
+
+import com.example.mediaplayerproject.model.Global;
+import com.example.mediaplayerproject.model.SearchDB;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CreateAndEditPlaylistController {
+     @FXML
+     private Label playlistName;
+     @FXML
+     private Button refreshButton;
+     @FXML
+     private Button deleteButton;
+     @FXML
+     private Button searchButton;
+     @FXML
+     private Button addItemButton;
+     @FXML
+     private Button removeItemButton;
+     @FXML
+     private Button updateButton;
+     @FXML
+     private Button createButton;
+     @FXML
+     private TextField searchField;
+     @FXML
+     private TextField nameOfPlaylist;
+     @FXML
+     private ListView allPlaylistsView;
+     @FXML
+     private ListView allMediaView;
+     @FXML
+     private ListView currentPlaylistView;
+
+     public void onButtonClickRefresh() throws SQLException {
+          allPlaylistsView.getItems().clear();
+          allMediaView.getItems().clear();
+          ResultSet resultSet = SearchDB.searchPlaylists();
+          while (resultSet.next()) {
+               allPlaylistsView.getItems().add(resultSet.getString(2));
+          }
+          for (int i = 0; i < Global.allMedia.size(); i++) {
+               allMediaView.getItems().add(Global.allMedia.get(i).getMediaTitle());
+          }
+     }
+     @FXML
+     public void plOnMouseClick(MouseEvent mouseEvent) throws SQLException {
+          currentPlaylistView.getItems().clear();
+          nameOfPlaylist.setText(allPlaylistsView.getSelectionModel().selectedItemProperty().getValue().toString());
+          ResultSet resultSet = SearchDB.searchMediaInPlaylist(allPlaylistsView.getSelectionModel().selectedItemProperty().getValue().toString());
+          Global.playlistMedia.clear();
+          while (resultSet.next()) {
+               Global.playlistMedia.add(resultSet.getString(5));
+
+          }
+          for (String s : Global.playlistMedia) {
+               currentPlaylistView.getItems().add(s);
+          }
+     }
+     public void onButtonClickSearch() {
+
+     }
+     public void onButtonClickDelete() {
+
+     }
+     public void onButtonClickUpdate() {
+
+     }
+     public void onButtonClickAdd() {
+
+     }
+     public void onButtonClickRemove() {
+
+     }
+     public void onButtonClickCreate() {
+
+     }
+
+}
