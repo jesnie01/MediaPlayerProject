309e2d6a984ed634fcbf7a345df5ae8a11b1022a
jesnie01 <jesperbnielsen@live.dk>
2024-01-25 11:27:27 +0100

    added HowToView and HowToContoller

---
+++ src/main/java/com/example/mediaplayerproject/controller/HowToController.java
@@ -0,0 +1,18 @@
+package com.example.mediaplayerproject.controller;
+import com.example.mediaplayerproject.model.GlobalInfo;
+import com.example.mediaplayerproject.model.SearchDB;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class HowToController {
+
+}
